88a58f089208c97e430e6873ee12114445e0bf8b
couscousdude <youwenw@gmail.com>
2020-08-08 03:26:55 -0700

    Updates

---
+++ precache-manifest.6adc8cfa899e2951a9c14653db093817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f0af50441f399b8032680ec338ef5a",
+    "url": "/help-hes-wack/index.html"
+  },
+  {
+    "revision": "517bcd5e207eb7068e5f",
+    "url": "/help-hes-wack/static/css/main.6227faed.chunk.css"
+  },
+  {
+    "revision": "701a3a1c5484f79ad46f",
+    "url": "/help-hes-wack/static/js/2.0c91ec65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/help-hes-wack/static/js/2.0c91ec65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517bcd5e207eb7068e5f",
+    "url": "/help-hes-wack/static/js/main.9f3e10b2.chunk.js"
+  },
+  {
+    "revision": "9c8bd39fb22c09870dfe",
+    "url": "/help-hes-wack/static/js/runtime-main.e70dec95.js"
+  },
+  {
+    "revision": "bbe97376d78d6dc494b814d6e9032709",
+    "url": "/help-hes-wack/static/media/Blackman.bbe97376.png"
+  }
+]);
